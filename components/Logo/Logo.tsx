6946ce25a60d1576dfb352efed72d4164ddeb363
Krystian Kopeć <73670385+Cannibal90@users.noreply.github.com>
2021-11-12 22:19:05 +0100

    comeback to add api key and upgrade dependencies

--- components/Logo/Logo.tsx
+++ components/Logo/Logo.tsx
@@ -1,51 +1,14 @@
 import React from "react";
-import { Text, View, StyleSheet, Image, TouchableOpacity } from "react-native";
+import { StyleSheet, Image } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
-import * as WebBrowser from "expo-web-browser";
-import {
-  makeRedirectUri,
-  useAuthRequest,
-  useAutoDiscovery,
-} from "expo-auth-session";
-
-WebBrowser.maybeCompleteAuthSession();
 
 const Logo = () => {
-  // Endpoint
-  const discovery = useAutoDiscovery(
-    "https://login.microsoftonline.com/ad60aea9-1995-4675-beb3-94257c7ffcd2/oauth2/v2.0"
-  );
-  // Request
-  const [request, response, promptAsync] = useAuthRequest(
-    {
-      clientId: "c45f77a3-807a-4952-bc85-5c4d5ca0173c",
-      scopes: [
-        "openid",
-        "profile",
-        "email",
-        "offline_access",
-        "api://digitalonus.oidccompliant/dou.read",
-      ],
-      redirectUri: makeRedirectUri({
-        native: "your.app",
-      }),
-    },
-    discovery
-  );
-
-  const fetchall = async () => {
-    const res = await promptAsync();
-    console.log(response);
-  };
-
   return (
     <LinearGradient
       colors={["#FC8E67", "#FDCC4E"]}
       style={styles.iconContainer}
     >
-      <TouchableOpacity disabled={!request} onPress={fetchall}>
-        <Image source={require("../../images/Logo.png")} style={styles.logo} />
-      </TouchableOpacity>
+      <Image source={require("../../images/Logo.png")} style={styles.logo} />
     </LinearGradient>
   );
 };
